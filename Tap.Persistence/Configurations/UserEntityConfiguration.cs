adc827d4bd269fee0d207c7acc30a9f332ab8552
LoaiMasri <loaimasri2002@gmail.com>
2023-12-08 17:37:12 +0200

    chore: reflect user table into database

--- Tap.Persistence/Configurations/UserEntityConfiguration.cs
+++ Tap.Persistence/Configurations/UserEntityConfiguration.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using Tap.Domain.Entities;
+using Tap.Domain.Features.Users;
 
 namespace Tap.Persistence.Configurations;
 
@@ -8,15 +8,23 @@ internal class UserEntityConfiguration : IEntityTypeConfiguration<User>
 {
     public void Configure(EntityTypeBuilder<User> builder)
     {
-        builder.ToTable("Users");
+        builder.ToTable(nameof(User));
 
         builder.HasKey(x => x.Id);
 
         builder.Property(x => x.Name).HasMaxLength(50).IsRequired();
 
-        builder.Property(x => x.Email).HasMaxLength(256).IsRequired();
+        builder
+            .Property(x => x.Email)
+            .HasConversion(x => x.Value, x => Email.Create(x).Value)
+            .HasMaxLength(Email.MaxLength)
+            .IsRequired();
 
-        builder.Property(x => x.Password).HasMaxLength(50).IsRequired();
+        builder
+            .Property(x => x.Password)
+            .HasConversion(x => x.Value, x => Password.Create(x).Value)
+            .HasMaxLength(Password.MaxLength)
+            .IsRequired();
 
         builder.Property(x => x.CreatedAtUtc).IsRequired();
 
