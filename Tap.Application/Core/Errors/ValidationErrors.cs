c0fbdf3c9c92e728b7a1b9aad678b79c9c38d446
LoaiMasri <loaimasri2002@gmail.com>
2023-12-08 17:39:27 +0200

    chore: add fluent validation,create custom error,exception

---
+++ Tap.Application/Core/Errors/ValidationErrors.cs
@@ -0,0 +1,20 @@
+﻿using Tap.Domain.Core.Primitives;
+
+namespace Tap.Application.Core.Errors;
+
+public static class ValidationErrors
+{
+    internal static class CreateUser
+    {
+        internal static Error NameIsRequired =>
+            new("CreateUser.NameIsRequired", "The name is required.");
+
+        internal static Error EmailIsRequired =>
+            new("CreateUser.EmailIsRequired", "The email is required.");
+
+        internal static Error PasswordIsRequired =>
+            new("CreateUser.PasswordIsRequired", "The password is required.");
+        internal static Error RoleIsRequired =>
+            new("CreateUser.RoleIsRequired", "The role is required.");
+    }
+}
