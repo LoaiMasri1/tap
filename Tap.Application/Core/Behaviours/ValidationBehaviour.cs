0e9ed921ea304d918268dad7db617f67a69af2b7
LoaiMasri <loaimasri2002@gmail.com>
2023-12-08 17:38:25 +0200

    chore: add validation,transaction behaviours

---
+++ Tap.Application/Core/Behaviours/ValidationBehaviour.cs
@@ -0,0 +1,43 @@
+﻿using FluentValidation;
+using MediatR;
+using Tap.Application.Core.Messaging;
+using ValidationException = Tap.Application.Core.Exceptions.ValidationException;
+
+namespace Tap.Application.Core.Behaviours;
+
+public class ValidationBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : class, IRequest<TResponse>
+    where TResponse : class
+{
+    private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+    public ValidationBehaviour(IEnumerable<IValidator<TRequest>> validators) =>
+        _validators = validators;
+
+    public async Task<TResponse> Handle(
+        TRequest request,
+        RequestHandlerDelegate<TResponse> next,
+        CancellationToken cancellationToken
+    )
+    {
+        if (request is IQuery<TResponse>)
+        {
+            return await next();
+        }
+
+        var context = new ValidationContext<TRequest>(request);
+
+        var failures = _validators
+            .Select(validator => validator.Validate(context))
+            .SelectMany(result => result.Errors)
+            .Where(failure => failure is not null)
+            .ToList();
+
+        if (failures.Any())
+        {
+            throw new ValidationException(failures);
+        }
+
+        return await next();
+    }
+}
