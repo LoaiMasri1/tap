d2722ff5d16bb663636902446f7573e242b53d9c
LoaiMasri <loaimasri2002@gmail.com>
2023-12-16 15:38:22 +0200

    refactor: refactor activate user command tests

--- Application.UnitTests/Features/Users/ActivateUserCommandTests.cs
+++ Application.UnitTests/Features/Users/ActivateUserCommandTests.cs
@@ -16,12 +16,15 @@ public class ActivateUserCommandTests
     private readonly IDateTime _dateTimeMock;
 
     private static readonly ActivateUserCommand Command = new("token");
+    private readonly ActivateUserCommandHandler _sut;
 
     public ActivateUserCommandTests()
     {
         _unitOfWorkMock = Substitute.For<IUnitOfWork>();
         _userRepositoryMock = Substitute.For<IUserRepository>();
         _dateTimeMock = Substitute.For<IDateTime>();
+
+        _sut = new ActivateUserCommandHandler(_userRepositoryMock, _unitOfWorkMock, _dateTimeMock);
     }
 
     [Fact]
@@ -30,14 +33,8 @@ public class ActivateUserCommandTests
         // Arrange
         _userRepositoryMock.GetByTokenAsync(Command.Token, default).Returns(Maybe<User>.None);
 
-        var sut = new ActivateUserCommandHandler(
-            _userRepositoryMock,
-            _unitOfWorkMock,
-            _dateTimeMock
-        );
-
         // Act
-        var result = await sut.Handle(Command, default);
+        var result = await _sut.Handle(Command, default);
 
         // Assert
         result.Error.Should().Be(DomainErrors.User.NotFound);
@@ -58,14 +55,8 @@ public class ActivateUserCommandTests
 
         _userRepositoryMock.GetByTokenAsync(Command.Token, default).Returns(user);
 
-        var sut = new ActivateUserCommandHandler(
-            _userRepositoryMock,
-            _unitOfWorkMock,
-            _dateTimeMock
-        );
-
         // Act
-        var result = await sut.Handle(Command, default);
+        var result = await _sut.Handle(Command, default);
 
         // Assert
         result.Error.Should().Be(DomainErrors.User.UserAllReadyActive);
@@ -75,28 +66,22 @@ public class ActivateUserCommandTests
     public async Task Handle_Should_ReturnError_When_TokenExpired()
     {
         // Arrange
+        var now = DateTime.UtcNow;
+
         var user = new User(
             "John Doe",
             Email.From("test@example.com"),
             "validDDD1!",
             UserRole.User,
-            new Token("token", DateTime.UtcNow.AddDays(1))
+            new Token("token", now.AddDays(1))
         );
-
-        var now = DateTime.UtcNow;
 
         _dateTimeMock.UtcNow.Returns(now.AddDays(2));
 
         _userRepositoryMock.GetByTokenAsync(Command.Token, default).Returns(user);
 
-        var sut = new ActivateUserCommandHandler(
-            _userRepositoryMock,
-            _unitOfWorkMock,
-            _dateTimeMock
-        );
-
         // Act
-        var result = await sut.Handle(Command, default);
+        var result = await _sut.Handle(Command, default);
 
         // Assert
         result.Error.Should().Be(DomainErrors.User.ActivationTokenExpired);
@@ -120,16 +105,37 @@ public class ActivateUserCommandTests
 
         _userRepositoryMock.GetByTokenAsync(Command.Token, default).Returns(user);
 
-        var sut = new ActivateUserCommandHandler(
-            _userRepositoryMock,
-            _unitOfWorkMock,
-            _dateTimeMock
-        );
-
         // Act
-        var result = await sut.Handle(Command, default);
+        var result = await _sut.Handle(Command, default);
 
         // Assert
         result.IsSuccess.Should().BeTrue();
+    }
+
+    [Fact]
+    public async Task Handle_Should_SaveChanges()
+    {
+        // Arrange
+        var now = DateTime.UtcNow;
+
+        var user = new User(
+            "John Doe",
+            Email.From("test@example.com"),
+            "validDDD1!",
+            UserRole.User,
+            new Token("token", now.AddDays(1))
+        );
+
+        // Act
+
+        _dateTimeMock.UtcNow.Returns(now.AddDays(1));
+
+        _userRepositoryMock.GetByTokenAsync(Command.Token, default).Returns(user);
+
+        await _sut.Handle(Command, default);
+
+        // Assert
+
+        await _unitOfWorkMock.Received(1).SaveChangesAsync();
     }
 }
