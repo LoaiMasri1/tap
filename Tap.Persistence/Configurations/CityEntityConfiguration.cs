8a5dcc0dda5520a96d358afa2656727e15978038
LoaiMasri1 <loaimasri2002@gmail.com>
2023-12-18 13:02:19 +0200

    #14 configure ef core for city and hotel

---
+++ Tap.Persistence/Configurations/CityEntityConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Tap.Domain.Features.Cities;
+
+namespace Tap.Persistence.Configurations;
+
+internal class CityEntityConfiguration : IEntityTypeConfiguration<City>
+{
+    public void Configure(EntityTypeBuilder<City> builder)
+    {
+        builder.ToTable(nameof(City));
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Name).HasMaxLength(50).IsRequired();
+
+        builder.Property(x => x.Description).HasMaxLength(500).IsRequired();
+
+        builder.Property(x => x.Country).HasMaxLength(50).IsRequired();
+
+        builder.Property(x => x.CreatedAtUtc).IsRequired();
+
+        builder.Property(x => x.UpdatedAtUtc);
+
+        builder
+            .HasMany(x => x.Hotels)
+            .WithOne()
+            .HasForeignKey(x => x.CityId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
