ddfd72be4f819635f18ac546f0b1752208a4c6dc
LoaiMasri1 <loaimasri2002@gmail.com>
2023-12-18 18:50:29 +0200

    #15 add role to identity user

--- Tap.Infrastructure/Authentication/UserIdentifierProvider.cs
+++ Tap.Infrastructure/Authentication/UserIdentifierProvider.cs
@@ -1,6 +1,7 @@
 ﻿using System.Security.Claims;
 using Microsoft.AspNetCore.Http;
 using Tap.Application.Features.Authentication;
+using Tap.Domain.Features.Users;
 
 namespace Tap.Infrastructure.Authentication;
 
@@ -15,7 +16,17 @@ public class UserIdentifierProvider : IUserIdentifierProvider
                     nameof(httpContextAccessor)
                 )
         );
+
+        Role = Enum.Parse<UserRole>(
+            httpContextAccessor.HttpContext?.User.FindFirst(ClaimTypes.Role)!.Value
+                ?? throw new ArgumentException(
+                    "The user role claim is required.",
+                    nameof(httpContextAccessor)
+                )
+        );
     }
 
     public int Id { get; }
+
+    public UserRole Role { get; }
 }
