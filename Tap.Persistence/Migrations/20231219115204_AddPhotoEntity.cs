748340c693c0c8c2b4cafdfa6831ba64c120b1ba
LoaiMasri1 <loaimasri2002@gmail.com>
2023-12-19 13:55:55 +0200

    #16 configure photo entity

---
+++ Tap.Persistence/Migrations/20231219115204_AddPhotoEntity.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tap.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPhotoEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Photo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Url = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    Type = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    ItemId = table.Column<int>(type: "int", nullable: false),
+                    CreatedAtUtc = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAtUtc = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Photo", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Photo_Type_ItemId",
+                table: "Photo",
+                columns: new[] { "Type", "ItemId" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Photo");
+        }
+    }
+}
---
+++ Tap.Persistence/Repositories/PhotoRepository.cs
@@ -0,0 +1,11 @@
+﻿using Tap.Application.Core.Abstractions.Data;
+using Tap.Domain.Core.Utility;
+using Tap.Domain.Features.Photos;
+
+namespace Tap.Persistence.Repositories;
+
+public class PhotoRepository : GenericRepository<Photo>, IPhotoRepository
+{
+    public PhotoRepository(IDbContext dbContext)
+        : base(dbContext) { }
+}
