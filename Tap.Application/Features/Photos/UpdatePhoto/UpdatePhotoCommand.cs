34dc4e7bfb39edd0c6ad7ee4bb47396058519fb2
LoaiMasri1 <loaimasri2002@gmail.com>
2023-12-21 12:27:11 +0200

    #16 updating photo

---
+++ Tap.Application/Features/Photos/UpdatePhoto/UpdatePhotoCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using Tap.Contracts.Features.Photos;
+using Tap.Contracts.Files;
+using Tap.Domain.Core.Primitives.Result;
+
+namespace Tap.Application.Features.Photos.UpdatePhoto;
+
+public record UpdatePhotoCommand(int PhotoId, FileRequest File)
+    : IRequest<Result<UpdatePhotoResponse>>;
---
+++ Tap.Application/Features/Photos/UpdatePhoto/UpdatePhotoCommandHandler.cs
@@ -0,0 +1,58 @@
+﻿using MediatR;
+using Tap.Application.Core.Abstractions.Common;
+using Tap.Application.Core.Abstractions.Data;
+using Tap.Contracts.Features.Photos;
+using Tap.Domain.Core.Errors;
+using Tap.Domain.Core.Primitives.Result;
+using Tap.Domain.Features.Photos;
+
+namespace Tap.Application.Features.Photos.UpdatePhoto;
+
+public class UpdatePhotoCommandHandler
+    : IRequestHandler<UpdatePhotoCommand, Result<UpdatePhotoResponse>>
+{
+    private readonly IPhotoRepository _photoRepository;
+    private readonly IUploadFileService _uploadFileService;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public UpdatePhotoCommandHandler(
+        IPhotoRepository photoRepository,
+        IUploadFileService uploadFileService,
+        IUnitOfWork unitOfWork
+    )
+    {
+        _photoRepository = photoRepository;
+        _uploadFileService = uploadFileService;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<Result<UpdatePhotoResponse>> Handle(
+        UpdatePhotoCommand request,
+        CancellationToken cancellationToken
+    )
+    {
+        var maybePhoto = await _photoRepository.GetByIdAsync(request.PhotoId, cancellationToken);
+
+        if (maybePhoto.HasNoValue)
+        {
+            return DomainErrors.Photo.PhotoNotFound;
+        }
+
+        var photo = maybePhoto.Value;
+
+        var fileResult = request.File.CreateFileRequest();
+
+        if (fileResult.IsFailure)
+        {
+            return fileResult.Error;
+        }
+
+        var fileResponse = await _uploadFileService.UploadFilesAsync(new[] { request.File });
+
+        photo.UpdateUrl(fileResponse[0].Url);
+
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return new UpdatePhotoResponse(photo.Url);
+    }
+}
