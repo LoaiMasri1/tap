a70072f390b7b3964377f01202c662cba85d6ca1
LoaiMasri1 <loaimasri2002@gmail.com>
2023-12-24 19:54:09 +0200

    #18 add amenities domain logic

---
+++ Tap.Domain/Features/Amenities/Amenity.cs
@@ -0,0 +1,51 @@
+﻿using Tap.Domain.Core.Abstraction;
+using Tap.Domain.Core.Errors;
+using Tap.Domain.Core.Primitives;
+using Tap.Domain.Core.Primitives.Result;
+using Tap.Domain.Core.Utility;
+
+namespace Tap.Domain.Features.Amenities;
+
+public class Amenity : Entity, IAuditableEntity
+{
+    private Amenity() { }
+
+    public string Name { get; private set; }
+    public string Description { get; private set; }
+    public AmenityType Type { get; private set; }
+    public int TypeId { get; private set; }
+    public DateTime CreatedAtUtc { get; private set; }
+    public DateTime? UpdatedAtUtc { get; private set; }
+
+    private Amenity(string name, string description, AmenityType type, int typeId)
+    {
+        Ensure.NotEmpty(name, "The name is required.", nameof(name));
+        Ensure.NotEmpty(description, "The description is required.", nameof(description));
+        Ensure.Of(type, "The type should be one of the following: Hotel, Room", nameof(type));
+        Ensure.NotDefault(typeId, "The type id is required.", nameof(typeId));
+
+        Name = name;
+        Description = description;
+        Type = type;
+        TypeId = typeId;
+    }
+
+    public static Amenity Create(string name, string description, AmenityType type, int typeId) =>
+        new(name, description, type, typeId);
+
+    public Result Update(string name, string description)
+    {
+        Ensure.NotEmpty(name, "The name is required.", nameof(name));
+        Ensure.NotEmpty(description, "The description is required.", nameof(description));
+
+        if (Name == name && Description == description)
+        {
+            return DomainErrors.Amenity.NothingToUpdate;
+        }
+
+        Name = name;
+        Description = description;
+
+        return Result.Success();
+    }
+}
