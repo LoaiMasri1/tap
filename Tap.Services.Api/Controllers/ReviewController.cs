26b098bd623d8a509bbbf9abe35b8aeff0a639d3
LoaiMasri1 <loaimasri2002@gmail.com>
2023-12-28 13:34:09 +0200

    #21 add endpoints for create,delete review

---
+++ Tap.Services.Api/Controllers/ReviewController.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Tap.Application.Features.Reviews.CreateReview;
+using Tap.Application.Features.Reviews.DeleteReview;
+using Tap.Contracts.Features.Reviews;
+using Tap.Domain.Core.Errors;
+using Tap.Domain.Core.Primitives.Result;
+using Tap.Services.Api.Contracts;
+using Tap.Services.Api.Infrastructure;
+
+namespace Tap.Services.Api.Controllers;
+
+[Authorize]
+public class ReviewController : ApiController
+{
+    [HttpPost(ApiRoutes.Review.Post)]
+    public async Task<IActionResult> CreateReview(
+        [FromQuery] [Required] int hotelId,
+        [FromQuery] [Required] int userId,
+        [FromBody] CreateReviewRequest request
+    ) =>
+        await Result
+            .Create((hotelId, userId, request))
+            .Ensure(x => x.hotelId == request.HotelId, DomainErrors.General.UnProcessableRequest)
+            .Ensure(x => x.userId == request.UserId, DomainErrors.General.UnProcessableRequest)
+            .Map(
+                x =>
+                    new CreateReviewCommand(
+                        x.request.Title,
+                        x.request.Content,
+                        x.request.Rating,
+                        x.hotelId,
+                        x.userId
+                    )
+            )
+            .Bind(x => Mediator.Send(x))
+            .Match(Ok, BadRequest);
+
+    [HttpDelete(ApiRoutes.Review.Delete)]
+    public async Task<IActionResult> DeleteReview(int id) =>
+        await Mediator.Send(new DeleteReviewCommand(id)).Match(Ok, BadRequest);
+}
