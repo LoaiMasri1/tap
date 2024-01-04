89fe2917ac89f63ed6998c6d48141a558b471375
LoaiMasri1 <loaimasri2002@gmail.com>
2024-01-04 12:45:10 +0200

    #22 get rooms case

---
+++ Tap.Application/Features/Rooms/GetRoomById/GetRoomByIdQuery.cs
@@ -0,0 +1,7 @@
+﻿using Tap.Application.Core.Messaging;
+using Tap.Contracts.Features.Rooms;
+using Tap.Domain.Core.Primitives.Maybe;
+
+namespace Tap.Application.Features.Rooms.GetRoomById;
+
+public record GetRoomByIdQuery(int Id) : IQuery<Maybe<FilteredRoomResponse>>;
